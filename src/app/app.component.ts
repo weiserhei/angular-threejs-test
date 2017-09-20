c2dd91a221b8a1660122fa2d1a7488131ca8ef64
weiserhei <falk@thiele-medien.de>
2017-09-20 23:00:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as THREE from 'three'
+//declare var THREE:any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(){
+    //called after the constructor and called  after the first ngOnChanges() 
+    var camera, scene, renderer;
+    var mesh;
+    init();
+    animate();
+    function init() {
+      camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+      camera.position.z = 400;
+      scene = new THREE.Scene();
+      var texture = new THREE.TextureLoader().load( './assets/textures/sprite-2x2.jpg' );
+      var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+      var material = new THREE.MeshBasicMaterial( { map: texture } );
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
+      //
+      window.addEventListener( 'resize', onWindowResize, false );
+    }
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+    function animate() {
+      requestAnimationFrame( animate );
+      mesh.rotation.x += 0.005;
+      mesh.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
+  }
+
+}
